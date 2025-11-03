176ef1a1876017126c829e9537e592d130b320dc
Aymane Berhili <aberhili0man@gmail.com>
2025-11-03 12:05:06 +0100

    add ComputeArray exercise solution

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,19 @@
+public class ComputeArray {
+
+    public static int[] computeArray(int[] array) {
+        if (array == null) {
+            return null;
+        }
+        int[] arr = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                arr[i] = array[i] * 5;
+            } else if ((array[i] - 1) % 3 == 0) {
+                arr[i] = array[i] + 7;
+            } else {
+                arr[i] = array[i];
+            }
+        }
+        return arr;
+    }
+}
