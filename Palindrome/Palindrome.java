38f6a4020331c2325f8570d882866bcba4cf613d
Aymane Berhili <aberhili0man@gmail.com>
2025-11-03 11:52:32 +0100

    add Palindrome exercise solution

---
+++ Palindrome/Palindrome.java
@@ -0,0 +1,14 @@
+
+public class Palindrome {
+
+    public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return false;
+        }
+        String rev = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+        return s.equals(rev);
+    }
+}
