9fdf59836f574dd910e724898b9c75cd3dd4fdfe
Aymane Berhili <aberhili0man@gmail.com>
2025-11-04 15:15:53 +0100

    add Capitalize exercise solution

--- Capitalize/Capitalize.java
+++ Capitalize/Capitalize.java
@@ -1,33 +1,28 @@
+
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Capitalize {
+
     public static void capitalize(String[] args) throws IOException {
-        if (args == null || args.length < 2) return;
-
-        try (Reader reader = new BufferedReader(new FileReader(args[0]));
-             Writer writer = new BufferedWriter(new FileWriter(args[1]))) {
-
-            boolean newWord = true;
-            int ch;
-            while ((ch = reader.read()) != -1) {
-                char c = (char) ch;
-
-                if (Character.isLetter(c)) {
-                    if (newWord) {
-                        c = Character.toUpperCase(c);
-                    } else {
-                        c = Character.toLowerCase(c);
-                    }
-                    newWord = false;
-                } else if (Character.isDigit(c)) {
-                    newWord = false;
-                } else {
-                    newWord = true;
-                }
-
-                writer.write(c);
-            }
-            writer.flush();
+        if (args.length != 2) {
+            return;
         }
+        String filename1 = args[0];
+        String filename2 = args[1];
+        Path path1 = Path.of(filename1);
+        Path path2 = Path.of(filename2);
+        String content = Files.readString(path1);
+        String[] words = content.trim().split("\\s+");
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            if (word.length() > 0) {
+                words[i] = Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
+            }
+        }
+        String result = String.join(" ", words);
+        Files.writeString(path2, result);
+
     }
 }
