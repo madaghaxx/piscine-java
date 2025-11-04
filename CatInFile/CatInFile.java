c847254acaccd1cb2c5c67463d7426e5b13de135
Aymane Berhili <aberhili0man@gmail.com>
2025-11-04 11:01:17 +0100

    add CatInFile exercise solution

--- CatInFile/CatInFile.java
+++ CatInFile/CatInFile.java
@@ -1,18 +1,17 @@
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class CatInFile {
+
     public static void cat(String[] args) throws IOException {
-        if (args == null || args.length == 0) return;
-        
-        byte[] buffer = new byte[8192];
-        for (String path : args) {
-            try (InputStream in = new BufferedInputStream(new FileInputStream(path))) {
-                int n;
-                while ((n = in.read(buffer)) != -1)
-                    System.out.write(buffer, 0, n);
-            }
+        if (args.length != 1) {
+            return;
         }
-        System.out.flush();
+        String filename = args[0];
+        Path path = Path.of(filename);
+        byte[] content = System.in.readAllBytes();
+        Files.write(path, content);
     }
 }
