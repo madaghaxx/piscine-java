03e1eb5a786c06f9401f6e23d2af9aa21fbf4888
Aymane Berhili <aberhili0man@gmail.com>
2025-11-05 18:32:04 +0100

    add StarOverride exercise solution

---
+++ StarOverride/Star.java
@@ -0,0 +1,37 @@
+
+public class Star extends CelestialObject {
+
+    private double magnitude;
+
+    public double getMagnitude() {
+        return this.magnitude;
+    }
+
+    public void setMagnitude(double test) {
+        this.magnitude = test;
+    }
+
+    public Star(String a, double b, double c, double d) {
+        this.name = a;
+        this.x = b;
+        this.y = c;
+        this.z = d;
+    }
+
+    public String toString() {
+        java.text.DecimalFormatSymbols sym = java.text.DecimalFormatSymbols.getInstance(java.util.Locale.US);
+        java.text.DecimalFormat df = new java.text.DecimalFormat("0.000", sym);
+        return this.name + " shines at the " + df.format(this.magnitude) + " magnitude";
+    }
+
+    public Boolean equals(Star object) {
+        if (object == null) {
+            return false;
+        }
+        return (this.x == object.x && this.y == object.y && this.z == object.z && this.name.equals(object.name) && this.magnitude == object.magnitude);
+    }
+
+    public int hashCode() {
+        return java.util.Objects.hash(x, y, z, name, magnitude);
+    }
+}
