c8a01d410e2bbf9df5c44af95bde9d554f820513
Aymane Berhili <aberhili0man@gmail.com>
2025-11-05 15:06:06 +0100

    add StarStatic exercise solution

---
+++ StarStatic/CelestialObject.java
@@ -0,0 +1,69 @@
+
+public class CelestialObject {
+
+    public double x, y, z;
+    public String name;
+
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = "Soleil";
+    }
+
+    public CelestialObject(String name, double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.name = name;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public double getZ() {
+        return this.z;
+    }
+
+    public void setX(double a) {
+        this.x = a;
+    }
+
+    public void setY(double a) {
+        this.y = a;
+    }
+
+    public void setZ(double a) {
+        this.z = a;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String a) {
+        this.name = a;
+    }
+
+    public static double getDistanceBetween(CelestialObject a, CelestialObject b) {
+        double xx, yy, zz;
+        xx = b.x - a.x;
+        yy = b.y - a.y;
+        zz = b.z - a.z;
+        xx *= xx;
+        yy *= yy;
+        zz *= zz;
+        double res = xx + yy + zz;
+        return Math.sqrt(res);
+    }
+
+    public double getDistanceBetweenInKm(CelestialObject a, CelestialObject b) {
+        return getDistanceBetween(a, b) * this.KM_IN_ONE_AU;
+    }
+    public double KM_IN_ONE_AU = 150000000;
+}
