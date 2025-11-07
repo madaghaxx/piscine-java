da1a1101db8b887149040d21e9a320b82f0f9442
Aymane Berhili <aberhili0man@gmail.com>
2025-11-07 18:33:15 +0100

    add StarMass exercise solution

---
+++ StarMass/Galaxy.java
@@ -0,0 +1,39 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Galaxy {
+
+    private List<CelestialObject> celestialObjects;
+
+    public Galaxy() {
+        this.celestialObjects = new ArrayList<>();
+    }
+
+    public List<CelestialObject> getCelestialObjects() {
+        return this.celestialObjects;
+    }
+
+    public void addCelestialObject(CelestialObject obj) {
+        this.celestialObjects.add(obj);
+    }
+
+    public Map<String, Integer> computeMassRepartition() {
+        Map<String, Integer> dictionnary = new HashMap<>();
+        dictionnary.put("Star", 0);
+        dictionnary.put("Other", 0);
+        dictionnary.put("Planet", 0);
+        for (CelestialObject obj : this.celestialObjects) {
+            if (obj instanceof Star) {
+                dictionnary.put("Star", dictionnary.get("Star") + obj.getMass());
+            } else if (obj instanceof Planet) {
+                dictionnary.put("Planet", dictionnary.get("Planet") + obj.getMass());
+            } else {
+                dictionnary.put("Other", dictionnary.get("Other") + obj.getMass());
+            }
+        }
+        return dictionnary;
+    }
+}
