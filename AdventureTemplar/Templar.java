639e0d9d6dd10b656c015c2e4a6fb19ca604b196
Aymane Berhili <aberhili0man@gmail.com>
2025-11-10 17:52:27 +0100

    wow

--- AdventureTemplar/Templar.java
+++ AdventureTemplar/Templar.java
@@ -35,7 +35,7 @@ public class Templar extends Character implements Healer, Tank {
     @Override
     public String toString() {
         if (this.getCurrentHealth() > 0) {
-            return String.format("%s  is a strong Templar with %d HP. It can heal %d HP and has a shield of %d.",
+            return String.format("%s is a strong Templar with %d HP. It can heal %d HP and has a shield of %d.",
                     this.getName(), this.getCurrentHealth(), this.getHealCapacity(), this.getShield());
         } else {
             return String.format("%s has been beaten, even with its %d shield. So bad, it could heal %s HP.",
