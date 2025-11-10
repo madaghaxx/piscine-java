66d38b40b6ac0724204f140d7beb72aff377666a
Aymane Berhili <aberhili0man@gmail.com>
2025-11-10 11:25:38 +0100

    add AdventureCharacter exercise solution

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,50 @@
+
+public class Character {
+
+    static int maxHealth;
+
+    public int getMaxHealth() {
+        return this.maxHealth;
+    }
+
+    int currentHealth;
+
+    public int getCurrentHealth() {
+        return this.currentHealth;
+    }
+
+    static String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Character(String nm, int max) {
+        this.maxHealth = max;
+        this.currentHealth = max;
+        this.name = nm;
+    }
+
+    @Override
+    public String toString() {
+        if (this.currentHealth > 0) {
+            return this.getName() + " : " + this.currentHealth + "/" + this.maxHealth;
+        } else {
+            return this.getName() + " : KO";
+        }
+    }
+
+    public void takeDamage(int sub) {
+        if (this.currentHealth - sub > 0) {
+
+            this.currentHealth -= sub;
+        } else {
+            this.currentHealth = 0;
+        }
+
+    }
+
+    public void attack(Character hero) {
+        hero.takeDamage(9);
+    }
+}
