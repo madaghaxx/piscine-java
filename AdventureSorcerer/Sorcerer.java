2d126629b6a8d00fe5741272620b368dc7b1b34d
Aymane Berhili <aberhili0man@gmail.com>
2025-11-10 17:00:57 +0100

    pushing Adventure

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -14,7 +14,7 @@ public abstract class Sorcerer extends Character implements Healer {
     }
 
     public void heal() {
-        if (this.getCurrentHealth() + this.getHealCapacity() > this.getMaxHealth()) {
+        if (this.getCurrentHealth() + this.getHealCapacity() >       this.getMaxHealth()) {
             this.setCurrentHealth(this.getMaxHealth());
         } else {
             this.setCurrentHealth(healCapacity);
