fe9599276fb67a9246f10b9a74bbd0e32f0e2dea
Aymane Berhili <aberhili0man@gmail.com>
2025-11-11 13:03:48 +0100

    pushing : AdventureCatch

--- AdventureCatch/Templar.java
+++ AdventureCatch/Templar.java
@@ -68,14 +68,10 @@ public class Templar extends Character implements Healer, Tank {
 
     public void takeDamage(int sub) throws DeadCharacterException {
         int newHealth;
-        newHealth = this.getCurrentHealth() - (sub - this.getShield());
-        if (newHealth > 0) {
-            this.setCurrentHealth(newHealth);
-        } else {
-            throw new DeadCharacterException(this);
-        }
         if (this.getCurrentHealth() <= 0) {
             throw new DeadCharacterException(this);
         }
+        newHealth = this.getCurrentHealth() - (sub - this.getShield());
+        this.setCurrentHealth(newHealth);
     }
 }
