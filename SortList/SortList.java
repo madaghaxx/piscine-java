7e9d3b8a5943576d9ac4af8fe2093ea2ed8e726c
Aymane Berhili <aberhili0man@gmail.com>
2025-11-12 18:53:14 +0100

    wow

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -3,11 +3,17 @@ import java.util.List;
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
-       list.sort(null);
-       return list;
+        if (list == null) {
+            return null;
+        }
+        list.sort(null);
+        return list;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
+        if (list == null) {
+            return null;
+        }
         list.sort((a, b) -> b - a);
         return list;
     }
