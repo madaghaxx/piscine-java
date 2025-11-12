f6e1ca88d2447a332fcba5bfe7dea54a578ea220
Aymane Berhili <aberhili0man@gmail.com>
2025-11-12 18:42:50 +0100

    wow

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -3,6 +3,9 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        return list1.equals(list2);
+        if (list1 != null && list2 != null) {
+
+            return list1.equals(list2);
+        }
     }
 }
