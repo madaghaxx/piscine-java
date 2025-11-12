75bc7d550941716375d84b9a89273e6647df57fe
Aymane Berhili <aberhili0man@gmail.com>
2025-11-12 19:12:22 +0100

    wow

---
+++ SetOperations/SetOperations.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetOperations {
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        if (set1 == null) {
+            return set2;
+        }
+        if (set2 == null) {
+            return set1;
+        }
+        Set<Integer> res = set1;
+        for (int i : set2) {
+            if (!set1.contains(i)) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    public static Set<Integer> intersection(Set<Integer> set1, Set<Integer> set2) {
+        if (set1 == null) {
+            return set2;
+        }
+        if (set2 == null) {
+            return set1;
+        }
+        Set<Integer> res = set1;
+        for (int i : set2) {
+            if (set1.contains(i)) {
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
