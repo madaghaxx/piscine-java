5784fd19187f5d568375cf564a022b7a40b546e2
Aymane Berhili <aberhili0man@gmail.com>
2025-11-12 18:23:38 +0100

    wow

--- ListContains/ListContains.java
+++ ListContains/ListContains.java
@@ -2,6 +2,6 @@ import java.util.List;
 
 public class ListContains {
     public static boolean containsValue(List<Integer> list, Integer value) {
-        list.contains(value);
+        return list.contains(value);
     }
 }
