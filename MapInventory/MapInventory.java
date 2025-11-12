1f6742906010a7960832d755f203fd7cf110881b
Aymane Berhili <aberhili0man@gmail.com>
2025-11-12 19:06:28 +0100

    wow

---
+++ MapInventory/MapInventory.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MapInventory {
+    public static int getProductPrice(Map<String, Integer> inventory, String productId) {
+        if (inventory == null) {
+            return -1;
+        }
+        return inventory.get(productId);
+    }
+
+    public static List<String> getProductIdsByPrice(Map<String, Integer> inventory, int price) {
+        if (inventory == null) {
+            return null;
+        }
+        List<String> result = new ArrayList<>();
+        inventory.forEach((product, Price) -> {
+            if (Price == price) {
+                result.add(product);
+            }
+        });
+        return result;
+    }
+}
