8f6a72b2451174cccb46aff1996067bfc8522c08
Aymane Berhili <aberhili0man@gmail.com>
2025-11-15 14:44:15 +0100

    pushing StreamMap

---
+++ StreamMap/StreamMap.java
@@ -0,0 +1,15 @@
+public class StreamMap {
+    public static Integer sumOfStringLength(Stream<String> s) {
+        int res;
+        s.map(a -> res += a.lentgh());
+        return res;
+    }
+
+    public static List<String> upperCaseAllString(Stream<String> s) {
+        return s.map(a -> a.toUpperCase());
+    }
+
+    public static Set<Integer> uniqIntValuesGreaterThan42(Stream<Double> s) {
+        return s.filter(a -> a > 42);
+    }
+}
