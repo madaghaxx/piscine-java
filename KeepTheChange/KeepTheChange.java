b846e356ba2b2778383436f297a2bf2719ce84a3
Aymane Berhili <aberhili0man@gmail.com>
2025-11-15 15:08:08 +0100

    pushing KeepTheChange

--- KeepTheChange/KeepTheChange.java
+++ KeepTheChange/KeepTheChange.java
@@ -2,17 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public static List<Integer> computeChange(int amount, Set<Integer> coins) {
-    List<Integer> res = new ArrayList<>();
-    List<Integer> sortedCoins = new ArrayList<>(coins);
-    sortedCoins.sort((a, b) -> b - a);
-    int i = 0;
-    while (amount > 0) {
-        if (amount % sortedCoins.get(i) > 1) {
-            amount -= sortedCoins.get(i);
-        } else {
-            i++;
+public class KeepTheChange {
+
+    public static List<Integer> computeChange(int amount, Set<Integer> coins) {
+        List<Integer> res = new ArrayList<>();
+        List<Integer> sortedCoins = new ArrayList<>(coins);
+        sortedCoins.sort((a, b) -> b - a);
+        int i = 0;
+        while (amount > 0) {
+            if (amount % sortedCoins.get(i) > 1) {
+                amount -= sortedCoins.get(i);
+            } else {
+                i++;
+            }
         }
     }
-
 }
