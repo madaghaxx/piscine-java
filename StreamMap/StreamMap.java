3b1220931daaad18aa533abb0d105f3960700ae7
Aymane Berhili <aberhili0man@gmail.com>
2025-11-15 14:48:33 +0100

    pushing StreamMap

--- StreamMap/StreamMap.java
+++ StreamMap/StreamMap.java
@@ -1,5 +1,5 @@
-import java.util.*;
 import java.util.stream.Stream;
+import java.util.*;
 public class StreamMap {
     public static Integer sumOfStringLength(Stream<String> s) {
         int res;
@@ -12,6 +12,6 @@ public class StreamMap {
     }
 
     public static Set<Integer> uniqIntValuesGreaterThan42(Stream<Double> s) {
-        return s.filter(a -> a > 42);
+        return s.filter(a -> a > 42).map(a -> a.intValue()).collect(Collectors.toSet());
     }
 }
