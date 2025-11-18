b87a83111c93ea8c17f8b402c318113f0644fc5c
Aymane Berhili <aberhili0man@gmail.com>
2025-11-18 16:25:28 +0100

    pushing DifferenceBetweenDate

--- DifferenceBetweenDate/DifferenceBetweenDate.java
+++ DifferenceBetweenDate/DifferenceBetweenDate.java
@@ -7,16 +7,25 @@ import java.time.Period;
 public class DifferenceBetweenDate {
 
     public static Duration durationBetweenTime(LocalTime localTime1, LocalTime localTime2) {
+        if (localTime1 == null || localTime2 == null) {
+            return null;
+        }
         Duration duration = Duration.between(localTime1, localTime2);
         return duration.isNegative() ? duration.negated() : duration;
     }
 
     public static Period periodBetweenDate(LocalDate date1, LocalDate date2) {
+        if (date1 == null || date2 == null) {
+            return null;
+        }
         Period period = Period.between(date1, date2);
         return period.isNegative() ? period.negated() : period;
     }
 
     public static Long numberOfHoursBetweenDateTime(LocalDateTime dateTime1, LocalDateTime dateTime2) {
+        if (dateTime1 == null || dateTime2 == null) {
+            return null;
+        }
         long hours = Duration.between(dateTime1, dateTime2).toHours();
         return Math.abs(hours);
     }
