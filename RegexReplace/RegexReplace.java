a7bcbcba2965d8f6d1813f0091303e74c7ba18bb
Aymane Berhili <aberhili0man@gmail.com>
2025-11-18 16:17:11 +0100

    pushing RegexReplace

--- RegexReplace/RegexReplace.java
+++ RegexReplace/RegexReplace.java
@@ -1,23 +1,25 @@
+package RegexReplace;
+
 public class RegexReplace {
     public static String removeUnits(String s) {
-        return s.replaceAll("(\\d)(cm|€)(?= |$)", "$1").trim();
-    }
+
     public static String obfuscateEmail(String s) {
         return s.replaceAll(
-            "([a-zA-Z0-9]+)([-._])([a-zA-Z0-9]+)@([a-zA-Z0-9.-]+)",
-            "$1$2***@$4"
-        ).replaceAll(
-            "\\b([a-zA-Z0-9]{1,3})([a-zA-Z0-9]{3,})@([a-zA-Z0-9.-]+)",
-            "$1***@$3"
-        ).replaceAll(
-            "@([a-zA-Z0-9]+)\\.([a-zA-Z0-9]+)\\.([a-zA-Z]{2,})",
-            "@***.$2.***"
-        ).replaceAll(
-            "@([a-zA-Z0-9]+)\\.((?!com|org|net)[a-zA-Z]{2,})\\b",
-            "@***.***"
-        ).replaceAll(
-            "@([a-zA-Z0-9]+)\\.(com|org|net)\\b",
-            "@*******.$2"
-        );
+                "([a-zA-Z0-9]+)([-._])([a-zA-Z0-9]+)@([a-zA-Z0-9.-]+)",
+                "$1$2***@$4").replaceAll(
+                        "\\b([a-zA-Z0-9]{1,3})([a-zA-Z0-9]+)@([a-zA-Z0-9.-]+)",
+                        "$1***@$3")
+                .replaceAll(
+                        "@([a-zA-Z0-9]+)\\.([a-zA-Z0-9]+)\\.([a-zA-Z0-9]+)\\.([a-zA-Z]{2,})",
+                        "@*******.$2.$3.***")
+                .replaceAll(
+                        "@([a-zA-Z0-9]+)\\.([a-zA-Z0-9]+)\\.([a-zA-Z]{2,})",
+                        "@***.$2.***")
+                .replaceAll(
+                        "@([a-zA-Z0-9]+)\\.((?!com|org|net)[a-zA-Z]{2,})\\b",
+                        "@***.***")
+                .replaceAll(
+                        "@([a-zA-Z0-9]+)\\.(com|org|net)\\b",
+                        "@*******.$2");
     }
 }
