b9028ea77c9eade4d1ce0576c91e45e2818940e2
Aymane Berhili <aberhili0man@gmail.com>
2025-11-18 12:38:12 +0100

    pushing FormatDate

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -1,5 +1,3 @@
-package FormatDate;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -17,12 +15,5 @@ public class FormatDate {
 
     public static String formatIso(LocalTime time) {
         return String.format("%d:%d:%d.%d",time.getHour(),time.getMinute(),time.getSecond(),time.getNano());
-    }
-
-    public static void main(String[] args) {
-        System.out.println(FormatDate.formatToFullText(LocalDateTime.of(2021, 8, 22, 13, 25, 46)));
-        System.out.println(FormatDate.formatSimple(LocalDate.of(2022, 2, 13)));
-        System.out.println(FormatDate.formatIso(LocalTime.of(16, 18, 56, 8495847)));
-
     }
 }
