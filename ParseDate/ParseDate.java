2baa7874a96fd861f20f0c12983ebf2dcf91ae24
Aymane Berhili <aberhili0man@gmail.com>
2025-11-18 11:38:47 +0100

    pushing ParseDate

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -3,16 +3,24 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class ParseDate {
-
     public static LocalDateTime parseIsoFormat(String stringDate) {
+        if (stringDate == null) {
+            return LocalDateTime.parse("");
+        }
         return LocalDateTime.parse(stringDate);
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
+         if (stringDate == null) {
+            return LocalDate.parse("");
+        }
         return LocalDate.parse(stringDate, java.time.format.DateTimeFormatter.ofPattern("EEEE, MMMM d, yyyy"));
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
+         if (stringDate == null) {
+            return LocalTime.parse("");
+        }
         return LocalTime.parse(stringDate);
     }
 }
