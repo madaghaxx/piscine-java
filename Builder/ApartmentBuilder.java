fadf62650703fc3609a45c1cdbdf096d73401228
Aymane Berhili <aberhili0man@gmail.com>
2025-11-19 15:04:17 +0100

    pushing Builder

--- Builder/ApartmentBuilder.java
+++ Builder/ApartmentBuilder.java
@@ -1,20 +1,28 @@
 class ApartmentBuilder implements HousingBuilder {
     public Apartment apartment;
 
+    public ApartmentBuilder() {
+        this.apartment = new Apartment();
+    }
+
     public HousingBuilder setSize(int size) {
         this.apartment.setSize(size);
+        return this;
     }
 
     public HousingBuilder setPrice(int price) {
         this.apartment.setPrice(price);
+        return this;
     }
 
     public HousingBuilder setRooms(int rooms) {
         this.apartment.setRooms(rooms);
+        return this;
     }
 
     public HousingBuilder setName(String name) {
         this.apartment.setName(name);
+        return this;
     }
 
     public Housing build() {
--- Builder/HouseBuilder.java
+++ Builder/HouseBuilder.java
@@ -1,20 +1,28 @@
 class HouseBuilder implements HousingBuilder {
     public House house;
 
+    public HouseBuilder() {
+        this.house = new House();
+    }
+
     public HousingBuilder setSize(int size) {
         this.house.setSize(size);
+        return this;
     }
 
     public HousingBuilder setPrice(int price) {
         this.house.setPrice(price);
+        return this;
     }
 
     public HousingBuilder setRooms(int rooms) {
         this.house.setRooms(rooms);
+        return this;
     }
 
     public HousingBuilder setName(String name) {
         this.house.setName(name);
+        return this;
     }
 
     public Housing build() {
